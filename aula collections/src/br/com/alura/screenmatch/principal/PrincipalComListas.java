05b9dfbf176ffe92d54a6d34ef16b2a8eaeea038
Tiago <tiagotaligon@gmail.com>
2023-07-19 18:02:23 -0300

    adicionando desafio final

---
+++ aula collections/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,45 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.*;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Serie lost = new Serie("Lost", 2000);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        Filme f1 = filmeDoPaulo;
+
+        List<Titulo> lista = new LinkedList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação " + filme.getClassificacao());
+            }
+        }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jaqueline");
+        System.out.println(buscaPorArtista);
+        Collections.sort(buscaPorArtista);
+        System.out.println(buscaPorArtista);
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println(lista);
+
+    }
+}
